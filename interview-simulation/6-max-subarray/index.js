b688ed410c27ceff88e1cc45bbdc4fe0075789b7
Vinicius Filenga <vinifilenga@hotmail.com>
2023-09-02 22:31:58 -0300

    --

---
+++ interview-simulation/6-max-subarray/index.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+
+  let eachIndexMaxes = {};
+
+  let max = 0;
+
+  for (var i = 0; i < nums.length; i++) {
+    eachIndexMaxes[i] = {}
+    for (var j = i + 1; j < nums.length; j++) {
+
+      const sumUntilPreviousIndex = eachIndexMaxes[i][j - 1];
+
+      if (sumUntilPreviousIndex === undefined) {
+        eachIndexMaxes[i][j] = nums[i] + nums[j];
+      }
+      else {
+        eachIndexMaxes[i][j] = sumUntilPreviousIndex + nums[j];
+      }
+
+      max = max > eachIndexMaxes[i][j] ? max : eachIndexMaxes[i][j]
+    }
+  }
+
+  return max;
+};
+
+const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// const nums = [5, 4, -1, 7, 8]
+console.log(maxSubArray(nums));
