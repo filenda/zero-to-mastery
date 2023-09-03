0d59549a49bd5ab16a42e79d4e9426b81a271d2d
Vinicius Filenga <vinifilenga@hotmail.com>
2023-09-03 13:27:06 -0300

    --

--- data-structures-and-algorithms/hashtable/js-sandbox.js
+++ data-structures-and-algorithms/hashtable/js-sandbox.js
@@ -38,15 +38,25 @@ class HashTable {
   }
 
   keys() {
-    const allKeys = []
-
+    if (!this.data.length) {
+      return undefined
+    }
+    let result = []
+    // loop through all the elements
     for (let i = 0; i < this.data.length; i++) {
-      if (this.data[i]) {
-        allKeys.push(this.data[i][0][0])
+      // if it's not an empty memory cell
+      if (this.data[i] && this.data[i].length) {
+        //TOCHECK: but also loop through all the potential collisions
+        if (this.data.length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            result.push(this.data[i][j][0])
+          }
+        } else {
+          result.push(this.data[i][0])
+        }
       }
     }
-
-    return allKeys;
+    return result;
   }
 }
 
--- interview-simulation/6-max-subarray/index.js
+++ interview-simulation/6-max-subarray/index.js
@@ -1,3 +1,5 @@
+//TODO
+
 /**
  * @param {number[]} nums
  * @return {number}
