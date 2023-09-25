85d89aca2c8f13f2562a06f2fc04e9027ad734ea
Vinicius Filenga <vinifilenga@hotmail.com>
2023-09-24 23:33:05 -0300

    feat: fr34king implemented merge sort and thus overcoming childhood early programming days trauma

--- algorithms/sorting/insertion/index.js
+++ algorithms/sorting/insertion/index.js
@@ -8,7 +8,8 @@ function insertionSort(array) {
       //move number to the first position
       array.unshift(array.splice(i, 1)[0]);
     } else {
-      // only sort number smaller than number on the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
+      // only sort number smaller than number on the left of it. 
+      // This is the part of insertion sort that makes it fast if the array is almost sorted.
       if (array[i] < array[i - 1]) {
         //find where number should go
         for (var j = 1; j < i; j++) {
--- algorithms/sorting/merge/index.js
+++ algorithms/sorting/merge/index.js
@@ -3,11 +3,25 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function mergeSort(array) {
-  if (array.length === 1) {
+  const length = array.length
+
+  if (length === 1) {
     return array
   }
 
   // Split Array in into right and left
+  const floorlyRoundedHalf = Math.floor(length / 2)
+  let right = []
+  let left = []
+
+  for (let i = 0; i < array.length; i++) {
+    if (i < floorlyRoundedHalf) {
+      left.push(array[i])
+    }
+    else {
+      right.push(array[i])
+    }
+  }
 
   return merge(
     mergeSort(left),
@@ -16,9 +30,29 @@ function mergeSort(array) {
 }
 
 function merge(left, right) {
+  let mergedArray = new Array(left.length + right.length)
 
+  let leftIndex = 0
+  let rightIndex = 0
+  let mergedArrayIndex = 0;
+
+  while (leftIndex < left.length || rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex] || right[rightIndex] === undefined) {
+      mergedArray[mergedArrayIndex] = left[leftIndex]
+      leftIndex++
+    }
+    else {
+      mergedArray[mergedArrayIndex] = right[rightIndex]
+      rightIndex++
+    }
+
+    mergedArrayIndex++
+  }
+
+  return mergedArray
 }
-
 
 const answer = mergeSort(numbers);
 console.log(answer);
+
+// console.log(merge([4], [0]))
