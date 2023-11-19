f1dc078b0c24f725b3646fa4c86e0d3491b3c66c
Vinicius Filenga <vinifilenga@hotmail.com>
2023-11-19 01:24:05 -0300

    shortest path problem

--- interview-simulation/18-shortest-path/index.ts
+++ interview-simulation/18-shortest-path/index.ts
@@ -5,40 +5,76 @@ interface OptimalRoute {
   totalTime: number;
 }
 
-//TODO: implement this shortest path problem
+//TODO: implement this shortest path problem yourself
+
 function optimizeDumpsterRoute(
   facilityLocation: number,
   locations: number,
   streets: Street[],
   dumpsters: number[]
 ): OptimalRoute {
-  let optimizedRoute: OptimalRoute = {
-    route: [],
-    totalTime: 0
+  const graph: Record<number, [number, number][]> = {};
+
+  // Build the graph
+  for (const [source, destination, time] of streets) {
+    if (!graph[source]) {
+      graph[source] = [];
+    }
+
+    graph[source].push([destination, time]);
   }
 
-  //Put the facility location as the first location of the optimzed route
-  optimizedRoute.route.push(facilityLocation)
+  const priorityQueue: [number, number, number[]][] = []; // [totalTime, location, route]
+  const totalTimeToLocation: Record<string, number> = {};
+  const visitedRoutes: Record<string, boolean> = {};
 
-  //Find the facility location
+  // Initialize starting location (facility)
+  priorityQueue.push([0, facilityLocation, [facilityLocation]]);
+  totalTimeToLocation[`${facilityLocation}-${facilityLocation}`] = 0;
 
-  // Loop through the dumpster locations
+  while (priorityQueue.length > 0) {
+    priorityQueue.sort((a, b) => a[0] - b[0]); // Sort the priority queue based on totalTime
+    const [currentTotalTime, currentLocation, currentRoute] = priorityQueue.shift()!;
+    console.log(currentTotalTime, currentLocation, currentRoute);
 
-  for (let i = 0; i < dumpsters.length; i++) {
-    // loop through the streets array
-    for (let j = 0; j < streets.length; j++) {
+    if (visitedRoutes[currentRoute.join('-')]) {
+      continue;
+    }
 
+    visitedRoutes[currentRoute.join('-')] = true;
+
+    if (dumpsters.findIndex(d => d === currentLocation) !== -1) {
+      dumpsters = dumpsters.filter((d) => d !== currentLocation);
+    }
+
+    if (dumpsters.length === 0) {
+      // If all dumpsters are picked up, return to the facility
+      const returnTime = graph[currentLocation]?.find(([destination]) => destination === facilityLocation)?.[1] || 0;
+      const totalTime = currentTotalTime + returnTime;
+      return { route: currentRoute.concat(facilityLocation), totalTime };
+    }
+
+    for (const [neighbor, time] of graph[currentLocation] || []) {
+      const newTotalTime = currentTotalTime + time;
+      const newRoute = currentRoute.concat(neighbor);
+
+      const key = newRoute.join('-');
+      if (
+        !totalTimeToLocation[key] ||
+        newTotalTime < totalTimeToLocation[key]
+      ) {
+        totalTimeToLocation[key] = newTotalTime;
+        priorityQueue.push([newTotalTime, neighbor, newRoute]);
+      }
     }
   }
 
-  //Put the facility location as the last location of the optimzed route
-  optimizedRoute.route.push(facilityLocation)
+  // Return an empty route if not all dumpsters are picked up
+  return { route: [], totalTime: 0 };
+}
 
-  return optimizedRoute
-};
-
-const facilityLocation = 1;
-const locations = 5;
+const facilityLocation = 1
+const locations = 5
 const streets: Street[] = [
   [1, 2, 5],
   [2, 3, 10],
@@ -49,9 +85,9 @@ const streets: Street[] = [
   [3, 2, 4],
   [4, 3, 2],
   [5, 4, 1]
-];
-const dumpsters = [2, 3, 4, 5];
+]
+const dumpsters = [2, 3, 4, 5]
 
-console.log(optimizeDumpsterRoute(facilityLocation, locations, streets, dumpsters));
+console.log(optimizeDumpsterRoute(facilityLocation, locations, streets, dumpsters))
 // Possible optimized route: [1, 2, 3, 4, 5, 1]
 // Total time could be 5 + 10 + 7 + 3 + 8 = 33
