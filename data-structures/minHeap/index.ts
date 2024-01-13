fb968671f176e2547a7cc52e78e067512a55a6fa
Vinicius Filenga <vinifilenga@hotmail.com>
2024-01-13 18:11:25 -0300

    feat: min heap - not implemented yet

---
+++ data-structures/minHeap/index.ts
@@ -0,0 +1,16 @@
+//Insert first element
+//Insert second element
+//Check whether the second element is smaller than the first
+//If so, swap elements
+//Take next (third an so on) element
+//Remember to follow the rule: left to right, top to bottom
+//Apply the same checks and swap elements
+
+//TODO: Implement this minheap yourself following the rules above
+class MinHeap{
+
+}
+
+const minHeap = new MinHeap()
+
+// minHeap.insert()
