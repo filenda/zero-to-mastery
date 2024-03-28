90110b4b2e51b6a28fdbf95fc5173d39c530e9eb
Vinicius Filenga <vinifilenga@hotmail.com>
2024-03-28 20:17:04 -0300

    feat: lc74 update

--- leetcode/74. Search a 2D Matrix/index.ts
+++ leetcode/74. Search a 2D Matrix/index.ts
@@ -1,6 +1,6 @@
 
-//TOCHECK: Took me a gross 38min to finish it (one test case only), using debug mode
-function searchMatrix(matrix: number[][], target: number): boolean {
+//TOCHECK: O(n) - 1st attempt - Took me a gross 38min to finish it (one test case only), using debug mode
+function searchMatrix1(matrix: number[][], target: number): boolean {
   //search for row
   let potentialRow = Math.trunc(matrix.length / 2)
 
@@ -37,6 +37,51 @@ function searchMatrix(matrix: number[][], target: number): boolean {
     }
     else// if (matrix[potentialRow][potentialColumn] === target) {
       return true
+  }
+
+  return false
+}
+
+//TOCHECK: O(log m + log n) - 2nd attempt - Took me a gross 28min to finish all base test cases
+function searchMatrix(matrix: number[][], target: number): boolean {
+  let top = 0
+  let bot = matrix.length - 1
+  let row = 0
+
+  while (bot >= top) {
+    row = Math.trunc((top + bot) / 2)
+
+    if (target > matrix[row][matrix[row].length - 1]) {
+      top = row + 1
+    }
+    else if (target < matrix[row][0]) {
+      bot = row - 1
+    }
+    else {
+      break
+    }
+  }
+
+  if (bot < top) {
+    return false
+  }
+
+  let first = 0
+  let last = matrix[row].length - 1
+  let col = 0
+
+  while (last >= first) {
+    col = Math.trunc((first + last) / 2)
+
+    if (target > matrix[row][col]) {
+      first = col + 1
+    }
+    else if (target < matrix[row][col]) {
+      last = col - 1
+    }
+    else {
+      return true
+    }
   }
 
   return false
